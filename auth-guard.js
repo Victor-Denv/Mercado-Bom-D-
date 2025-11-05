66939d2b5c39a61ff31e305f47cf4c96d74300bd
Victor-Denv <vida12345678904@gmail.com>
2025-11-05 02:45:00 -0300

    Remove página de login e redireciona para a página inicial após autenticação

--- auth-guard.js
+++ auth-guard.js
@@ -8,5 +8,5 @@ const isAuthenticated = localStorage.getItem('userToken'); // MUDANÇA AQUI
 if (!isAuthenticated) {
     // 3. Se não existe, o usuário é expulso para a tela de login
     alert('Você precisa estar logado para acessar esta página.');
-    window.location.href = 'login.html';
+    window.location.href = 'index.html';
 }
